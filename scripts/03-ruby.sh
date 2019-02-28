e38ad7c0b9c92d08f249e44ec7cf23d5b3a514d2
aikuma0130 <ktaka0130@gmail.com>
2019-02-28 06:27:34 -0800

    Add ruby,nodejs install script

---
+++ scripts/03-ruby.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eu
+
+source ~/.bash_profile
+
+rbenv install 2.4.5
+
+rbenv global 2.4.5
+
+gem install neovim
---
+++ scripts/04-nodejs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source ~/.bash_profile
+
+if ! ndenv install -l >/dev/null 2>&1; then
+    git clone https://github.com/riywo/node-build $(ndenv root)/plugins/node-build
+fi
+
+ndenv install v10.15.2
+
+ndenv global v10.15.2
+
+npm install -g neovim
