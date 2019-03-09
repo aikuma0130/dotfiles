39b5fe6cb8ab9495dee58bc42edff257cbb6e7ae
Dakie <ktaka0130@gmail.com>
2019-03-09 23:00:00 +0900

    Fix scroll direction

--- scripts/00-osx-defaults.sh
+++ scripts/00-osx-defaults.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Disable “natural” (Lion-style) scrolling
-defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+# Enable "natural" (Lion-style) scrolling
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool ture
 
